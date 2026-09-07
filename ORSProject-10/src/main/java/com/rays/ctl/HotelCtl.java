0933ba4059f0e1146fb05bbf200a3167e9bb173d
abhi070717 <abhibhawsar066@gmail.com>
2026-09-07 18:19:21 +0530

    Project 10 backend

---
+++ ORSProject-10/src/main/java/com/rays/ctl/HotelCtl.java
@@ -0,0 +1,31 @@
+package com.rays.ctl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rays.common.BaseCtl;
+import com.rays.common.ORSResponse;
+import com.rays.dto.HotelDTO;
+import com.rays.form.HotelForm;
+import com.rays.service.HotelServiceInt;
+
+@RestController
+@RequestMapping(value = "Hotel")
+public class HotelCtl extends BaseCtl<HotelForm, HotelDTO, HotelServiceInt> {
+
+	@Autowired
+	private HotelServiceInt hotelService;
+
+	@GetMapping("preload")
+	public ORSResponse preload() {
+		ORSResponse res = new ORSResponse(true);
+		HotelDTO dto = new HotelDTO();
+		List<HotelDTO> list = hotelService.search(dto, userContext);
+		res.addResult("hotelList", list);
+		return res;
+	}
+}
