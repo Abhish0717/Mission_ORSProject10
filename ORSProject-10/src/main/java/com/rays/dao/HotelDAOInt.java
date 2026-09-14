7001f5c7e9e64ff90e690cd59b2c47dc966c88ea
abhi070717 <abhibhawsar066@gmail.com>
2026-09-14 13:34:51 +0530

    Update

--- ORSProject-10/src/main/java/com/rays/dao/HotelDAOInt.java
+++
@@ -1,8 +0,0 @@
-package com.rays.dao;
-
-import com.rays.common.BaseDAOInt;
-import com.rays.dto.HotelDTO;
-
-public interface HotelDAOInt extends BaseDAOInt<HotelDTO> {
-
-}
--- ORSProject-10/src/main/java/com/rays/email/EmailServiceInt.java
+++
@@ -1,6 +0,0 @@
-package com.rays.email;
-
-public interface EmailServiceInt {
-
-	public void sendMail(EmailMessage msg);
-}
